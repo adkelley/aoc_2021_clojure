e7c5791ecf1c0d8c5671dd4c2dc584fcbbcb841e
Alex Kelley <alexkelley@Alexs-MBP.webpass.net>
2021-12-22 07:12:11 -0800

    Completed data->lines

--- 05-aoc/src/aoc/core.clj
+++ 05-aoc/src/aoc/core.clj
@@ -1,4 +1,6 @@
-(ns aoc.core)
+(ns aoc.core
+  (:require
+   [clojure.string :as str]))
 
 (def example
 "0,9 -> 5,9
@@ -11,5 +13,49 @@
 3,4 -> 1,4
 0,0 -> 8,8
 5,5 -> 8,2")
+
+(def input (slurp "resources/input.txt"))
+
+(defn data->lines
+  "Return collection of integers representing line
+  segments x1, y1, x2, y2 . . ."
+  [data]
+  (->> data
+       str/split-lines
+       (mapcat #(re-seq #"\d+" %))
+       (map #(Integer/parseInt %))))
+
+(defonce example->lines (data->lines example))
+(defonce input->lines (data->lines input))
+
+;; process lines
+(defn line-segment
+  "Return first 4 integers (line segment) from lines"
+  [lines]
+  (take 4 lines))
+
+(defn x1
+  "Return x1 from line-segment x1, y1, x2, y2"
+  [line]
+  (first line))
+
+(defn y1
+  "Return y1 from line-segment x1, y1, x2, y2"
+  [line]
+  (second line))
+
+(defn x2
+  "Return x2 from line-segment x1, y1, x2, y2"
+  [line]
+  (nth line 2))
+
+(defn y2
+  "Return y2 from line-segment x1, y1, x2, y2"
+  [line]
+  (nth line 3))
+
+
+(nth (line-segment example->lines) 2)
+
 
 
