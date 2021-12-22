1fcf9d008d9e3ff8f113803444bbe82d8e3afca7
Alex Kelley <alexkelley@Alexs-MBP.webpass.net>
2021-12-22 14:40:01 -0800

    Completed grid construction

--- 05-aoc/src/aoc/core.clj
+++ 05-aoc/src/aoc/core.clj
@@ -23,39 +23,137 @@
   (->> data
        str/split-lines
        (mapcat #(re-seq #"\d+" %))
-       (map #(Integer/parseInt %))))
+       (map #(Integer/parseInt %))
+       (partition 4)))
 
 (defonce example->lines (data->lines example))
 (defonce input->lines (data->lines input))
 
 ;; process lines
-(defn line-segment
+
+(defn first-line
   "Return first 4 integers (line segment) from lines"
   [lines]
-  (take 4 lines))
+  (first lines))
 
-(defn x1
+(defn first-x
   "Return x1 from line-segment x1, y1, x2, y2"
   [line]
   (first line))
 
-(defn y1
+(defn first-y
   "Return y1 from line-segment x1, y1, x2, y2"
   [line]
   (second line))
 
-(defn x2
+(defn last-x
   "Return x2 from line-segment x1, y1, x2, y2"
   [line]
   (nth line 2))
 
-(defn y2
+(defn last-y
   "Return y2 from line-segment x1, y1, x2, y2"
   [line]
   (nth line 3))
 
 
-(nth (line-segment example->lines) 2)
+(defn vertical-line?
+  ""
+  [line]
+  (= (first-x line) (last-x line)))
 
+(vertical-line? '(0 1 0 3)) ;; => true
 
+(defn horizontal-line?
+  ""
+  [line]
+  (= (first-y line) (last-y line)))
 
+(horizontal-line? '(0 1 3 1)) ;; => true
+
+(defn diagonal-line?
+  ""
+  [line]
+  (not (or (vertical-line? line) (horizontal-line? line))))
+
+(diagonal-line? '(0 1 3 3)) ;; => true
+
+(defn non-diagnonal-lines
+  "Return non-diagnonal lines from lines"
+  [lines]
+  (filter #(not (diagonal-line? %)) lines))
+
+(non-diagnonal-lines example->lines)
+
+(defn new-point
+  "Return map {:x x, :y y, :id hash :danger 0}"
+  [x y]
+  (let [id (hash (str x y))]
+    {:x x :y y :id id :danger 0}))
+
+(map #(new-point % 0) (range 0 (inc 2)))
+
+(defn make-horiz-points
+  ""
+  [line]
+  (let [y  (first-y line)
+        x1 (first-x line)
+        x2 (last-x  line)] 
+    (if (> x1 x2)
+      (map #(new-point % y) (range x2 (inc x1)))
+      (map #(new-point % y) (range x1 (inc x2))))))
+
+(make-horiz-points '(0 1 3 1))
+
+(defn make-vertical-points
+  ""
+  [line]
+  (let [x  (first-x line)
+        y1 (first-y line)
+        y2 (last-y  line)] 
+    (if (> y1 y2)
+      (map #(new-point x %) (range y2 (inc y1)))
+      (map #(new-point x %) (range y1 (inc y2))))))
+
+(make-vertical-points '(0 1 0 3)) 
+
+(defn make-points
+  ""
+  [line]
+  (if (vertical-line? line)
+    (make-vertical-points line)
+    (make-horiz-points line)))
+
+(make-points '(0 1 0 3))
+
+(defn make-grid
+  ""
+  [lines]
+  (reduce #(concat %1 (make-points %2)) {} lines))
+
+(make-grid (non-diagnonal-lines example->lines))
+
+(defn set-dangerous
+  ""
+  [grid]
+  grid)
+
+  
+;; Appendix
+(defn max-xs
+  ""
+  [lines]
+  (reduce max (take-nth 2 lines)))
+
+(defn max-ys
+  ""
+  [lines]
+  (reduce max (take-nth 2 (drop 1 lines))))
+
+(defn grid-size
+  ""
+  [lines]
+  (* (max-xs lines) (max-ys lines)))
+
+(grid-size example->lines)
+(grid-size input->lines)
