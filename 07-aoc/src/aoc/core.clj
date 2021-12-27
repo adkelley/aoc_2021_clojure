eec598747e3fde93a0d41f9ddb3aeb9a8805ed50
Alex Kelley <alexkelley@Alexs-MBP.webpass.net>
2021-12-27 07:31:33 -0800

    Day 7, completed initial-vec

--- 07-aoc/src/aoc/core.clj
+++ 07-aoc/src/aoc/core.clj
@@ -1,8 +1,41 @@
 (ns aoc.core
   (:require
-   [java.io]))
+   [clojure.java.io :as io]))
 
 (def example
   "16,1,2,0,4,2,7,1,2,14")
 
-(def input)
+(def input (slurp (io/resource "input.txt")))
+
+(defn data->vector
+  "Return [Long] from input data"
+  [data]
+  (->> data
+       (re-seq #"\d+")
+       (mapv #(Integer/parseInt %))))
+
+(defonce example->vector (data->vector example))
+(defonce input->vector (data->vector input))
+
+(defn max-num
+  "Return max number in collection"
+  [coll]
+  (reduce max coll))
+
+(defn min-num
+  "Return min number in collection"
+  [coll]
+  (reduce min coll))
+
+(max-num (data->vector example)) ;; => 16
+(min-num (data->vector example)) ;; => 0
+(max-num (data->vector input)) ;; => 1954
+(min-num (data->vector input)) ;; => 0
+
+(defn initial-vec
+  "Return vector of [] equal to length of p0 to pn"
+  [coll]
+  (let [length (inc (- (max-num coll) (min-num coll)))]
+    (vec (take length (repeat [])))))
+
+(initial-vec example->vector)
